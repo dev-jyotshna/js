19df118eeabe337fa9a05d323312353708b0efd2
Jyotshna Trivedi <98551142+dev-jyotshna@users.noreply.github.com>
2024-08-16 18:50:10 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// JS is dynamically typed language
+// Datatypes category : based on how they are stored and accessed
+// Primitives(call-by-value or copied) : 7 types -> String, Number, Boolean, null, undefined, Symbol, BigInt
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+console.log(typeof outsideTemp);
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 234567893456789067890244558888n
+console.log(typeof bigNumber);
+
+
+
+// Non-Primitive(call_by-reference or use original variable) : Array, Objects, Functions
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObject = {
+    name : "dangal",
+    age : 5
+}
+const myFunc = function(){
+    console.log("Hello");
+}
+console.log(typeof heros);
+
+
+// typeof:
+// null -> object
+// undefined -> undefined
+// Symbol -> symbol
+// bigInt -> bigint
+// array -> object
+// object -> object
+// function -> function  (here object function)
