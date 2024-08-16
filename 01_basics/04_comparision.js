9bca3d4e2e890513adc44d99328f5dce987898d4
Jyotshna Trivedi <98551142+dev-jyotshna@users.noreply.github.com>
2024-08-16 18:13:40 +0000

    checked comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);  
+// console.log("02" > 1);
+// true
+// true
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// false
+// false
+// true
+// false
+// false
+// false
+
+// ===  strict check : checks value and datatype
+console.log("2" == 2);
+console.log("2" === 2);
