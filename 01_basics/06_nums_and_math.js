4b34ab646b03cd47e9ccb077527c0c529555a76c
Jyotshna Trivedi <98551142+dev-jyotshna@users.noreply.github.com>
2024-08-18 17:46:40 +0000

    num b/w range

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,30 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8977
+// console.log(otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+/****************** MATHS ************/
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4.3, 5.6, 8, 1.4));
+// console.log(Math.max(4.3, 5.6, 8, 1.4));
+
+console.log(Math.random()); // value in between 0 - 1
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
