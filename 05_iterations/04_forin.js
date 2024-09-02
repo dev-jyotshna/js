d3059265385ab7f4907ceda4f92978a99fe7c09c
Jyotshna Trivedi <98551142+dev-jyotshna@users.noreply.github.com>
2024-09-02 19:19:43 +0000

    added for adv

---
+++ 05_iterations/04_forin.js
@@ -0,0 +1,27 @@
+//*** FOR IN
+const myObject = {
+    js : 'javascript',
+    cpp : 'C++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+}
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);     // console.log(myObject[key]); 
+// }
+
+// const programming = ["js", "rb", "py", "java", "cpp"]
+// for (const key in programming) {
+//     console.log(programming[key])
+// }
+
+// Maps are NOT iterable
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('FR', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
