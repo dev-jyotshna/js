6fb38bef079ac3efd7ba022e5dc3edb84ad26b19
Jyotshna Trivedi <98551142+dev-jyotshna@users.noreply.github.com>
2024-09-02 14:42:50 +0000

    added for and while

---
+++ 05_iterations/02_dowhile.js
@@ -0,0 +1,21 @@
+// ** WHILE
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index += 2
+// }
+
+// let myArray = ['Ironman', 'spiderman', 'Hulk']
+// let i = 0
+// while(i < myArray.length){
+//     console.log(`Value is ${myArray[i]}`);
+//     i++
+// }
+
+// ** DO - WHILE
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
